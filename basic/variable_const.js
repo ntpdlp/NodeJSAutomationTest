7dd778520fc3df139b7556f98165e7be5fff42a7
ntpdlp <ntpdlp@gmail.com>
2020-04-28 12:51:14 -0500

    variable , const

---
+++ basic/variable_const.js
@@ -0,0 +1,20 @@
+// *********** Variable *********** //
+//number
+let myNumber =1;
+myNumber =2;
+console.log(myNumber);
+
+//string
+let myStr = "toi ten \"VoNguyen\"";
+console.log(myStr);
+
+// boolean
+let isRight = true;
+let isWrong = false;
+
+// *********** Constant *********** //
+const PI = 3.14;
+const MONTHLY_SALARY = 5000;
+
+
+
